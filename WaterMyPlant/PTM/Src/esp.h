311c900bf59a71561f591aef3316cb79752fb6c9
Błażej <44238271+BlaiseCz@users.noreply.github.com>
2019-04-01 20:11:48 +0200

    esp.h dodanie funkcji

--- WaterMyPlant/PTM/Src/esp.h
+++ WaterMyPlant/PTM/Src/esp.h
@@ -1,6 +1,14 @@
 #include "stm32f4xx_hal.h"
 #include "main.h"
-#include "sender.h"
+
+/*
+ * plik do obslugi polecen wysylaych na ESP 8266
+ *
+ *
+ * ---- TODO ----
+ *  1. Dopisac funkcje sendUART, ktora przyjmuje parametry, wielkosc tablicy i tresc przesylanej wadomosci
+ *  2. Dopisac dzialajaca funkcje //void ConnectToWiFi(UART_HandleTypeDef *huart2, char* ssid, char* pass) po to, zeby podawac rozne WiFi
+ */
 
 
 /*
@@ -12,22 +20,39 @@
  *  2. Dopisac dzialajaca funkcje //void ConnectToWiFi(UART_HandleTypeDef *huart2, char* ssid, char* pass) po to, zeby podawac rozne WiFi
  */
 
+/*
+ * Function prototypes
+ */
+
+
+void check(uint delay);
+void AT_command(UART_HandleTypeDef *huart2);
+void AT_reset(UART_HandleTypeDef *huart2);
+void AT_Set_WorkMode(UART_HandleTypeDef *huart2);
+void AT_MultipleConnections(UART_HandleTypeDef *huart2);
+void AT_Connect_To_Server(UART_HandleTypeDef *huart2);
+void AT_Connect_To_WiFi(UART_HandleTypeDef *huart2);
+void AT_Send(UART_HandleTypeDef *huart2);
+void AT_GET(UART_HandleTypeDef *huart2);
+
 //configuracja polaczenia esp z serwerem
 void ConfigESP(UART_HandleTypeDef *huart2)
 {
 	AT_command(huart2);
 //	AT_reset(huart2);
 //	AT_command(huart2);
-	AT_Set_ClientMode(huart2);
+	AT_Set_WorkMode(huart2);
 	AT_MultipleConnections(huart2);
 //	AT_Connect_To_WiFi(huart2);
 	AT_Connect_To_Server(huart2);
+	AT_Send(huart2);
+	AT_GET(huart2);
 }
 //sprawdzenie czy polaczono z ESP 8266 AT
 void AT_command(UART_HandleTypeDef *huart2)
 {
-	uint8_t sendUART[4] = {"AT\r\n"};
-	uint16_t sizeSendUART = 4;
+	uint8_t sendUART[40] = {"AT\r\n"};
+	uint16_t sizeSendUART = 40;
 	HAL_UART_Transmit_IT(huart2, sendUART, sizeSendUART);
 	check(300);
 }
@@ -45,7 +70,7 @@ void AT_reset(UART_HandleTypeDef *huart2)
  * 2 – Access Point (punkt dostępu)
  * 3 – Klient + Access Point
  */
-void AT_Set_ClientMode(UART_HandleTypeDef *huart2)
+void AT_Set_WorkMode(UART_HandleTypeDef *huart2)
 {
 	uint8_t sendUART[13] = {"AT+CWMODE=1\r\n"};
 	uint16_t sizeSendUART = 13;
@@ -60,32 +85,38 @@ void AT_MultipleConnections(UART_HandleTypeDef *huart2)
 	HAL_UART_Transmit_IT(huart2, sendUART, sizeSendUART);
 	check(300);
 }
-
 //kanal, protokol, adres, port
 void AT_Connect_To_Server(UART_HandleTypeDef *huart2)
 {
-	uint8_t sendUART[63] = {"AT+CIPSTART=4,\"TCP\",\"http://sim900automation.comli.com\",80\r\n"};
-	uint16_t sizeSendUART = 63;
+	uint8_t sendUART[100] = {"AT+CIPSTART=4,\"TCP\",\"krzysztof.r.czarnecki.student.put.poznan.pl\",80\r\n"};
+	uint16_t sizeSendUART = 100;
 	HAL_UART_Transmit_IT(huart2, sendUART, sizeSendUART);
-	check(10000);
+	check(2000);
 }
-
 
 //trzeba zrobic konkatenacje, zeby moc podawac dowolne parametry ssid i hasla do wifi
 //void ConnectToWiFi(UART_HandleTypeDef *huart2, char* ssid, char* pass)
 void AT_Connect_To_WiFi(UART_HandleTypeDef *huart2)
 {
-	//AT+CIPSTART="TCP","http://sim900automation.comli.com",80
-		//AT+CWJAP='ssid','pass' -> laczy z wifi
 		uint8_t sendUART[32] = {"AT+CWJAP=\"Krzysio\",\"misio1234\"\r\n"};
 		uint16_t sizeSendUART = 32;
 		HAL_UART_Transmit_IT(huart2, sendUART, sizeSendUART);
-		check(10000);
+		check(7000);
 }
 
-void AT_GET()
+void AT_Send(UART_HandleTypeDef *huart2)
 {
-	//GET /api/flower/1<CR><LF>Host:water-test2.herokuapp.com<CR><LF>
+		uint8_t sendUART[100] = {"AT+CIPSEND=4,129\r\n"};
+		uint16_t sizeSendUART = 100;
+		HAL_UART_Transmit_IT(huart2, sendUART, sizeSendUART);
+		check(2000);
+}
+void AT_GET(UART_HandleTypeDef *huart2)
+{
+		uint8_t sendUART[150] = {"GET /add_flower.php?flowerName=C0009 HTTP/1.1\r\nHost:krzysztof.r.czarnecki.student.put.poznan.pl\r\n\r\n\r\n\r\n"};
+		uint16_t sizeSendUART = 150;
+		HAL_UART_Transmit_IT(huart2, sendUART, sizeSendUART);
+		check(19000);
 }
 
 void AT_POST()
